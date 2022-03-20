07c43953ba3f3032aa62cee88291eed963e2404c
Stanislav Korolev <stan.korolev.qa@gmail.com>
2022-03-20 14:45:05 -0400

    Negative Tests

---
+++ src/test/java/NegativeTests.java
@@ -0,0 +1,17 @@
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class NegativeTests {
+    public static Response response;
+
+
+
+}
